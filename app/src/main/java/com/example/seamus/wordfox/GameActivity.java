30b64c035a17788bd63c2133c380c3784462ef39
seamuspg <seamus.g91@gmail.com>
2017-07-01 17:02:36 +0100

    Each cell is only clickable once. Reset/submit enables clickable again

--- app/src/main/java/com/example/seamus/wordfox/GameActivity.java
+++ app/src/main/java/com/example/seamus/wordfox/GameActivity.java
@@ -21,127 +21,134 @@ import java.util.Random;
 
 public class GameActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-        public static final String MONITOR_TAG = "myTag";
-        private TextView resetTV;
-        private TextView submitTV;
-        private TextView shuffleTV;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_game);
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
+    public static final String MONITOR_TAG = "myTag";
+    private TextView resetTV;
+    private TextView submitTV;
+    private TextView shuffleTV;
 
-            ArrayList givenLetters = new ArrayList();
-            givenLetters = getGivenLetters();
-            String givenLettersSTR = "";
-            for (int i=0; i<givenLetters.size(); i++){
-                givenLettersSTR += givenLetters.get(i);
-            }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-            TextView givenLettersTV = (TextView) findViewById(R.id.givenLettersGameScreen);
-            givenLettersTV.setText(givenLettersSTR);
-
-            writeToGuessGrid(givenLetters);
-
-            resetTV = (TextView) findViewById(R.id.resetButton);
-            submitTV = (TextView) findViewById(R.id.submitButton);
-            shuffleTV = (TextView) findViewById(R.id.shuffleButton);
-// consider changing to xml function onClick
-            resetTV.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                        TextView rst = (TextView) findViewById(R.id.currentAttempt);
-                        rst.setText("");
-                    }
-                    return true;
-                }
-
-            });
-
-
-            submitTV.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
-                        TextView currentTV = (TextView) findViewById(R.id.currentAttempt);
-                        String currentStr = (String) currentTV.getText();
-                        int currentStrLen = currentStr.length();
-
-                        TextView longestTV = (TextView) findViewById(R.id.longestAttempt);
-                        String longestStr = (String) longestTV.getText();
-                        int longestStrLen = longestStr.length();
-
-                        currentTV.setText("");
-
-                        if (currentStrLen > longestStrLen)
-                        {
-                            longestTV.setText(currentStr);
-
-                            String currentStrLenSTR = Integer.toString(currentStrLen);
-                            TextView lengthLongestTV = (TextView) findViewById(R.id.lengthLongestAttempt);
-                            lengthLongestTV.setText(currentStrLenSTR);
-                        }else{
-                            //provide the same feedback as if the user has enterred an incorrect word (one that isn't in the dictionary)
-                            // buzz vibrate and blink the screen or something
-                        }
-                    }
-                    return true;
-                }
-            });
-            shuffleTV.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
-                    }
-                    return true;
-                }
-            });
-
-
-
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-            drawer.setDrawerListener(toggle);
-            toggle.syncState();
-
-            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-            navigationView.setNavigationItemSelectedListener(this);
+        ArrayList givenLetters = new ArrayList();
+        givenLetters = getGivenLetters();
+        String givenLettersSTR = "";
+        for (int i = 0; i < givenLetters.size(); i++) {
+            givenLettersSTR += givenLetters.get(i);
         }
 
-    public boolean writeToGuessGrid(ArrayList<String> givenLetters){
+        TextView givenLettersTV = (TextView) findViewById(R.id.givenLettersGameScreen);
+        givenLettersTV.setText(givenLettersSTR);
 
-        TextView cell1 = (TextView) findViewById(R.id.guessGridCell1);
-        TextView cell2 = (TextView) findViewById(R.id.guessGridCell2);
-        TextView cell3 = (TextView) findViewById(R.id.guessGridCell3);
-        TextView cell4 = (TextView) findViewById(R.id.guessGridCell4);
-        TextView cell5 = (TextView) findViewById(R.id.guessGridCell5);
-        TextView cell6 = (TextView) findViewById(R.id.guessGridCell6);
-        TextView cell7 = (TextView) findViewById(R.id.guessGridCell7);
-        TextView cell8 = (TextView) findViewById(R.id.guessGridCell8);
-        TextView cell9 = (TextView) findViewById(R.id.guessGridCell9);
-        cell1.setText(givenLetters.get(0));
-        cell2.setText(givenLetters.get(1));
-        cell3.setText(givenLetters.get(2));
-        cell4.setText(givenLetters.get(3));
-        cell5.setText(givenLetters.get(4));
-        cell6.setText(givenLetters.get(5));
-        cell7.setText(givenLetters.get(6));
-        cell8.setText(givenLetters.get(7));
-        cell9.setText(givenLetters.get(8));
+        writeToGuessGrid(givenLetters);
+
+        resetTV = (TextView) findViewById(R.id.resetButton);
+        submitTV = (TextView) findViewById(R.id.submitButton);
+        shuffleTV = (TextView) findViewById(R.id.shuffleButton);
+// consider changing to xml function onClick
+        resetTV.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    TextView rst = (TextView) findViewById(R.id.currentAttempt);
+                    rst.setText("");
+
+                    setGridClickable();
+                }
+                return true;
+            }
+
+        });
+
+
+        submitTV.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                    TextView currentTV = (TextView) findViewById(R.id.currentAttempt);
+                    String currentStr = (String) currentTV.getText();
+                    int currentStrLen = currentStr.length();
+
+                    TextView longestTV = (TextView) findViewById(R.id.longestAttempt);
+                    String longestStr = (String) longestTV.getText();
+                    int longestStrLen = longestStr.length();
+
+                    currentTV.setText("");
+
+                    if (currentStrLen > longestStrLen) {
+                        longestTV.setText(currentStr);
+
+                        String currentStrLenSTR = Integer.toString(currentStrLen);
+                        TextView lengthLongestTV = (TextView) findViewById(R.id.lengthLongestAttempt);
+                        lengthLongestTV.setText(currentStrLenSTR);
+                    } else {
+                        //provide the same feedback as if the user has enterred an incorrect word (one that isn't in the dictionary)
+                        // buzz vibrate and blink the screen or something
+                    }
+
+                    setGridClickable();
+                }
+                return true;
+            }
+        });
+        shuffleTV.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                }
+                return true;
+            }
+        });
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    public boolean writeToGuessGrid(ArrayList<String> givenLetters) {
+
+        for (int i = 0; i < givenLetters.size(); i++) {
+            String allCellId = "guessGridCell" + (i + 1);
+            int resID = getResources().getIdentifier(allCellId, "id", getPackageName());
+            TextView currentCell = (TextView) findViewById(resID);
+
+            currentCell.setText(givenLetters.get(i));
+        }
 
         return true;
     }
 
+    public void setGridClickable() {
+        int cellCount = 9;
+        for (int i = 0; i < cellCount; i++) {
+
+            String allCellId = "guessGridCell" + (i + 1);
+
+            int resID = getResources().getIdentifier(allCellId, "id", getPackageName());
+            TextView currentCell = (TextView) findViewById(resID);
+            currentCell.setClickable(true);
+        }
+
+    }
 
     public void gridCellClicked(View v) {
 
+        // ArrayList<Integer> CellIDsUsed = new ArrayList<Integer>();
+
         String cellID = getResources().getResourceName(v.getId());
         int resID = getResources().getIdentifier(cellID, "id", getPackageName());
+
         TextView cellGridTV = (TextView) findViewById(resID);
         String cellLetter = (String) cellGridTV.getText();
 
@@ -150,6 +157,7 @@ public class GameActivity extends AppCompatActivity
 
         currentGuess += cellLetter;
         currentGuessTV.setText(currentGuess);
+        cellGridTV.setClickable(false);
         Log.d(MONITOR_TAG, "v ID: " + v.getId());
         Log.d(MONITOR_TAG, "ID: " + cellID);
         Log.d(MONITOR_TAG, "res ID: " + resID);
@@ -158,7 +166,7 @@ public class GameActivity extends AppCompatActivity
 
     public ArrayList<String> getGivenLetters() {
         ArrayList<String> givenLetters = new ArrayList<String>();
-        for (int i=0; i<9; i++) {
+        for (int i = 0; i < 9; i++) {
             givenLetters.add(randLetter());
         }
         return givenLetters;
@@ -172,6 +180,7 @@ public class GameActivity extends AppCompatActivity
         char ranChar = (char) alphabet.charAt(charIndex);
         return Character.toString(ranChar);
     }
+
     /////////////////////////////////
     @Override
     public void onBackPressed() {
@@ -229,7 +238,6 @@ public class GameActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
 
 
 }
