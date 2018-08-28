9fc4e074e657859031f05579af11c843ee71ea1c
Seamus G <seamus.g91@gmail.com>
2018-08-28 12:01:36 +0100

    Now giving the player a 'rank' on the final results screen. This indicates how well the player did. Greatly improved the appearance of the final results screen. Shows each players profile image and words found by highlighting them on a game grid. Removed unnecessary margin on data page.

--- app/src/main/java/com/example/seamus/wordfox/results_screen/RoundnGameResults.java
+++ app/src/main/java/com/example/seamus/wordfox/results_screen/RoundnGameResults.java
@@ -1,10 +1,21 @@
 package com.example.seamus.wordfox.results_screen;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,12 +23,15 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,19 +39,29 @@ import android.widget.Toast;
 import com.example.seamus.wordfox.FoxUtils;
 import com.example.seamus.wordfox.GameData;
 import com.example.seamus.wordfox.GameInstance;
+import com.example.seamus.wordfox.GridImage;
+import com.example.seamus.wordfox.ImageHandler;
 import com.example.seamus.wordfox.MainActivity;
 import com.example.seamus.wordfox.NavigationBurger;
 import com.example.seamus.wordfox.R;
 import com.example.seamus.wordfox.database.FoxSQLData;
 import com.example.seamus.wordfox.game_screen.GameActivity;
 import com.example.seamus.wordfox.player_switch.PlayerSwitchActivity;
+import com.example.seamus.wordfox.profile.FoxRank;
 import com.example.seamus.wordfox.profile.ProfileActivity;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RoundnGameResults extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
         ResultsContract.View {
+    private static final String DEFAULT_PROFILE_IMAGE_ASSET = "default_profile_smiley.png";
+    private static final int MAX_RESOLUTION_IMAGE = 2048;   // Max allowed picture resolution
 
     private NavigationBurger navBurger = new NavigationBurger();
 
@@ -56,6 +80,8 @@ public class RoundnGameResults extends AppCompatActivity
     private String p1Name = "";
     Button endOfRoundOrGameButton;
 
+    LayoutInflater resultInflater;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,20 +94,29 @@ public class RoundnGameResults extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_end_screen);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToHome();
+            }
+        });
+
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
 
         String roundOrGameEnd = getIntent().getExtras().getString("key");
         boolean gameOver = (roundOrGameEnd.equals("game"));
         gameIndexNumber = getIntent().getExtras().getInt("gameIndexNumber");
 
         lp = new LinearLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT); // width is first then height
-        resultsLL = (LinearLayout) findViewById(R.id.resultEndScreenLL);
-        wordsLL = (LinearLayout) findViewById(R.id.wordsEndScreenLL);
-        resultSectionEndScreenLL = (LinearLayout) findViewById(R.id.resultSectionEndScreenLL);
-
-        endOfRoundOrGameButton = (Button) findViewById(R.id.endOfRoundOrGameButton);
+//        resultsLL = (LinearLayout) findViewById(R.id.resultEndScreenLL);
+//        wordsLL = (LinearLayout) findViewById(R.id.wordsEndScreenLL);
+//        resultSectionEndScreenLL = (LinearLayout) findViewById(R.id.resultSectionEndScreenLL);
+//
+//        endOfRoundOrGameButton = (Button) findViewById(R.id.endOfRoundOrGameButton);
 
         ArrayList<GameInstance> instancesToDisplay = new ArrayList<>();
         if (gameOver) {
@@ -90,17 +125,135 @@ public class RoundnGameResults extends AppCompatActivity
             instancesToDisplay.add(MainActivity.allGameInstances.get(gameIndexNumber));
         }
         presenter = new ResultsPresenter(this, gameOver, MainActivity.allGameInstances.size(), new FoxSQLData(this), instancesToDisplay);
-        presenter.populateHeaderMsg();
-        presenter.populateResultLL();
-        presenter.endOfRoundOrGameResults();
-        presenter.createRoundSummary();
+//        presenter.populateHeaderMsg();
+//        presenter.populateResultLL();
+//        presenter.endOfRoundOrGameResults();
+//        presenter.createRoundSummary();
         presenter.updateData();
+
+
+        /////////
+        LinearLayout resultContainer = findViewById(R.id.player_results_container);
+        resultInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        for (int i = 0; i < MainActivity.allGameInstances.size(); ++i) {
+            resultContainer.addView(inflatePlayerResult(MainActivity.allGameInstances.get(i)));
+        }
+        GameInstance gameInstance = MainActivity.allGameInstances.get(0);
+        TextView best1 = findViewById(R.id.bestword_heading_1);
+        TextView best2 = findViewById(R.id.bestword_heading_2);
+        TextView best3 = findViewById(R.id.bestword_heading_3);
+        best1.setText(gameInstance.getRoundLongestPossible(0).toUpperCase());
+        best2.setText(gameInstance.getRoundLongestPossible(1).toUpperCase());
+        best3.setText(gameInstance.getRoundLongestPossible(2).toUpperCase());
+        String winner = "";
+        int highScore = -1;
+        for (int i = 0; i < MainActivity.allGameInstances.size(); ++i) {
+            if (MainActivity.allGameInstances.get(i).getTotalScore() > highScore) {
+                highScore = MainActivity.allGameInstances.get(i).getTotalScore();
+                winner = MainActivity.allGameInstances.get(i).getPlayerID();
+            }
+        }
+        TextView winnerText = findViewById(R.id.winner_banner_text);
+        winner = "Winner is " + winner + "!";
+        winnerText.setText(winner);
     }
+
+    private LinearLayout inflatePlayerResult(GameInstance gameInstance) {
+        LinearLayout cl = (LinearLayout) resultInflater.inflate(R.layout.result_player_layout, null);
+
+        GameData plyrGd = new GameData(this, gameInstance.getPlayerName());
+
+        String profPicStr = plyrGd.getProfilePicture();
+        Bitmap profPic = null;
+        ImageHandler imageHandler = new ImageHandler(this);     // Handle this better
+        if (!profPicStr.equals("")) {
+            Uri myFileUri = Uri.parse(profPicStr);
+            profPic = imageHandler.getBitmapFromUri(myFileUri, ImageHandler.dp2px(this, 60));
+        }
+        int maxScore = gameInstance.getHighestPossibleScore();
+        int playerScore = gameInstance.getTotalScore();
+        int percentScore = (100 * playerScore) / (maxScore);
+
+        TextView resultPlayerNameView = cl.findViewById(R.id.result_player_name);
+        String playerName = gameInstance.getPlayerID() + "  (" + percentScore + "%)";
+        resultPlayerNameView.setText(playerName);
+        TextView resultPlayerScoreView = cl.findViewById(R.id.result_player_score);
+        String playerResult = playerScore + " out of " + maxScore;
+        resultPlayerScoreView.setText(playerResult);
+        ImageView resultPlayerFoxPicView = cl.findViewById(R.id.result_player_fox_pic);
+        TextView resultPlayerRankNameView = cl.findViewById(R.id.result_player_rank_name);
+        FoxRank foxRank = determineRank(playerScore);
+        resultPlayerRankNameView.setText(foxRank.foxRank);
+
+
+        CircleImageView profilePicView = cl.findViewById(R.id.results_screen_profile_pic);
+        if (profPic == null) {
+            profilePicView.setVisibility(View.GONE);
+            resultPlayerNameView.setPadding(ImageHandler.dp2px(this, 20), ImageHandler.dp2px(this, 10), 10, 10);
+            resultPlayerScoreView.setPadding(ImageHandler.dp2px(this, 20), 10, 10, ImageHandler.dp2px(this, 10));
+        } else {
+            profilePicView.setImageBitmap(profPic);
+        }
+        profPic = null;
+        Bitmap foxRankImage = ImageHandler.getScaledBitmap(foxRank.imageResource, 120, getResources());
+//        Bitmap foxRankImage = BitmapFactory.decodeResource(getResources(), foxRank.imageResource);      // TODO: Re-use fox pics if players get the same rank
+        foxRankImage = ImageHandler.getResizedBitmap(foxRankImage, ImageHandler.dp2px(this, 60), ImageHandler.dp2px(this, 60));  // TODO: Adjust to screen size
+        resultPlayerFoxPicView.setImageBitmap(foxRankImage);
+        for (int i = 0; i < GameInstance.NUMBER_ROUNDS; ++i) {
+            String gridTag = "player_grid" + (i + 1);
+            String wordTag = "player_word" + (i + 1);
+            ImageView gridImage = cl.getChildAt(0).findViewWithTag(gridTag);
+            String bestWord = gameInstance.getRoundWord(i);
+            String letters = gameInstance.getLetters(i);
+            TextView wordTV = cl.getChildAt(0).findViewWithTag(wordTag);
+            wordTV.setText(bestWord);
+            gridImage.setImageBitmap(pressedKey(letters, bestWord));
+        }
+        return cl;
+    }
+
+    private FoxRank determineRank(int score) {
+        float avg = score / 3;
+        int rankValue = Math.round(avg);
+        switch (rankValue) {
+            case 3:
+                return new FoxRank(R.drawable.onefox_silhouette, "Red Fox");
+            case 4:
+                return new FoxRank(R.drawable.arcticfox, "Arctic Fox");
+            case 5:
+                return new FoxRank(R.drawable.onefox_silhouette, "Red Fox");
+            case 6:
+                return new FoxRank(R.drawable.arcticfox, "Arctic Fox");
+            case 7:
+                return new FoxRank(R.drawable.onefox_silhouette, "Red Fox");
+            case 8:
+                return new FoxRank(R.drawable.arcticfox, "Arctic Fox");
+            case 9:
+                return new FoxRank(R.drawable.onefox_silhouette, "Red Fox");
+            default:
+                return new FoxRank(R.drawable.onefox_trans, "Gray Fox");
+        }
+    }
+
+    private Bitmap pressedKey(String letters, String word) {
+        if (letters.equals(GameData.NONE_FOUND)) {    // TODO: Throw exception
+            return null;
+        }
+        if (word.equals(GameData.NONE_FOUND)) {
+            return null;
+        }
+        Bitmap buttongGridImage = BitmapFactory.decodeResource(getResources(), R.drawable.letter_grid_blank);
+        buttongGridImage = ImageHandler.getResizedBitmap(buttongGridImage, ImageHandler.dp2px(this, 100), ImageHandler.dp2px(this, 100));  // TODO: Adjust to screen size
+
+        GridImage grid = new GridImage(buttongGridImage, word, letters, getResources().getColor(R.color.game_font_color), getResources().getColor(R.color.colorLightAccent));
+        return grid.getBmp();
+    }
+
 
     @Override
     public void setGameOverMessage(String gameOverMessage) {
-        TextView gameOverTV = (TextView) findViewById(R.id.gameOverEndScreenTV);
-        gameOverTV.setText(gameOverMessage);
+//        TextView gameOverTV = (TextView) findViewById(R.id.gameOverEndScreenTV);
+//        gameOverTV.setText(gameOverMessage);
     }
 
     @Override
@@ -110,22 +263,26 @@ public class RoundnGameResults extends AppCompatActivity
 
     @Override
     public void setVictoryMessage(String victoryMessage) {
-        TextView winnerTV = new TextView(this);
-        winnerTV.setText(victoryMessage);
-        winnerTV.setGravity(Gravity.CENTER);
-        //add the textview with the winner's name into the LinearLayout containing the Game Over message
-        LinearLayout gameOverWinnerLL = findViewById(R.id.gameOverWinnerLLEndScreen);
-        winnerTV.setLayoutParams(lp);
-        gameOverWinnerLL.addView(winnerTV);
+//        TextView winnerTV = new TextView(this);
+//        winnerTV.setText(victoryMessage);
+//        winnerTV.setGravity(Gravity.CENTER);
+//        //add the textview with the winner's name into the LinearLayout containing the Game Over message
+//        LinearLayout gameOverWinnerLL = findViewById(R.id.gameOverWinnerLLEndScreen);
+//        winnerTV.setLayoutParams(lp);
+//        gameOverWinnerLL.addView(winnerTV);
     }
 
     @Override
     public void prepareHomeButton() {
         endOfRoundOrGameButton.setText(R.string.home_button_text);
         endOfRoundOrGameButton.setOnClickListener(v -> {
-            Intent MainIntent = new Intent(v.getContext(), MainActivity.class);
-            startActivity(MainIntent);
+            navigateToHome();
         });
+    }
+
+    public void navigateToHome(){
+        Intent MainIntent = new Intent(this, MainActivity.class);
+        startActivity(MainIntent);
     }
 
     @Override
@@ -225,7 +382,7 @@ public class RoundnGameResults extends AppCompatActivity
 
     @Override
     public String defaultP1Name() {
-        if(p1Name.equals("")){
+        if (p1Name.equals("")) {
             GameData fox = new GameData(this, GameData.DEFAULT_P1_NAME);
             p1Name = fox.getUsername();
         }
