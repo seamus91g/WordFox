4eddc4206a85f238e003f1988aaacb7d5044659b
Seamus G <seamus.g91@gmail.com>
2018-09-09 10:48:42 +0100

    Added variable vowel/consonant distribution. Previously was always 3:6. Now, 1/6 times it is 2:7 and 1/6 times it is 4:5. Rest is 3:6

--- app/src/main/java/com/example/seamus/wordfox/data/FoxDictionary.java
+++ app/src/main/java/com/example/seamus/wordfox/data/FoxDictionary.java
@@ -70,11 +70,11 @@ public class FoxDictionary implements Diction {
 
     public FoxDictionary(String validWordsFileName, String letterDistributionFile, AssetManager assetManager) {
         col = Collator.getInstance(new Locale("en", "EN"));
-        loadWords(validWordsFileName, letterDistributionFile, assetManager );
+        loadWords(validWordsFileName, letterDistributionFile, assetManager);
         resetLetterPool();
     }
 
-    public static void loadWords(String validWordsFileName, String letterDistributionFile, AssetManager assetManager){
+    public static void loadWords(String validWordsFileName, String letterDistributionFile, AssetManager assetManager) {
         if (allValidWords.isEmpty()) {
             try {
                 InputStream myIpStr = assetManager.open(validWordsFileName);
@@ -84,7 +84,7 @@ public class FoxDictionary implements Diction {
                 ioe.printStackTrace();
             }
         }
-        if (letterDistributionMap.isEmpty()){
+        if (letterDistributionMap.isEmpty()) {
             try {
                 InputStream myIpStr = assetManager.open(letterDistributionFile);
                 populateLetterDistribution(myIpStr);
@@ -96,9 +96,10 @@ public class FoxDictionary implements Diction {
         isWordListLoaded = true;
     }
 
-    public void resetLetterPool(){
+    public void resetLetterPool() {
         letterPool = new LetterPool(letterDistributionMap);
     }
+
     // Read in text file of list of words.
     // Also store in a Hashmap where each key is a word with its letters sorted alphabetically.
     // The Hashmap value for each key is the word itself. This is used to search for longest word
@@ -109,13 +110,14 @@ public class FoxDictionary implements Diction {
         while (readString != null) {
             String thisWord = readString.toLowerCase();
             String[] wordArray = thisWord.split(" ");
-            if (validWordsAlphabeticalKey.get(wordArray[1]) == null){
+            if (validWordsAlphabeticalKey.get(wordArray[1]) == null) {
                 validWordsAlphabeticalKey.put(wordArray[1], wordArray[0]);
             }
             allValidWords.add(wordArray[0]);
             readString = buffreader.readLine();
         }
     }
+
     // Store how many of each letter to use. Example, 8 letter Ts, 3 letter Bs, 1 letter Z
     private static void populateLetterDistribution(InputStream myIpStr) throws IOException {
         Reader reader = new InputStreamReader(myIpStr);
@@ -137,6 +139,7 @@ public class FoxDictionary implements Diction {
         }
         return wordAlphSorted;
     }
+
     public boolean checkWordExists(String checkWord) {
         if (allValidWords.contains(checkWord)) {
             return true;
@@ -144,10 +147,12 @@ public class FoxDictionary implements Diction {
             return false;
         }
     }
+
     // Find longest word from the random 9 game letters.
-        public ArrayList<String> longestWordFromLetters(String givenLetters) {
-            return longestWordFromLetters(givenLetters, 7);     // Default is 7
-        }
+    public ArrayList<String> longestWordFromLetters(String givenLetters) {
+        return longestWordFromLetters(givenLetters, 7);     // Default is 7
+    }
+
     public ArrayList<String> longestWordFromLetters(String givenLetters, int numberToSave) {
         givenLetters = makeStringAlphabetical(givenLetters.toLowerCase());
 
@@ -167,27 +172,28 @@ public class FoxDictionary implements Diction {
                 listOfLongest = substringSearch(givenLetters, listOfLongest, x, substringsChecked, numberToSave);
                 multipleLengths.addAll(listOfLongest);
                 if (numberToSave != 1 && listOfLongest.size() > 0) {
-                    numberToSave -= listOfLongest.size() -1;
+                    numberToSave -= listOfLongest.size() - 1;
                     numberToSave = numberToSave < 1 ? 1 : numberToSave;
                 }
-            }else{
+            } else {
                 Log.d(MONITOR_TAG_FOX, "=== BREAKING === at " + (9 - x) + " letters ---");
                 break;
             }
         }
         return multipleLengths;
     }
+
     // Recursively search for valid substrings
     private ArrayList<String> substringSearch(String givenLetters, ArrayList<String> knownLongest, int Depth, HashMap substringsChecked, int howMany) {
         ArrayList<String> thisLongest = knownLongest;
         int len = givenLetters.length();
-        for (int j = 0; j < len; j++){
+        for (int j = 0; j < len; j++) {
             String checkWindow = givenLetters.substring(0, j) + givenLetters.substring(j + 1, len);    // skip j
-            if (substringsChecked.containsKey(checkWindow)){
+            if (substringsChecked.containsKey(checkWindow)) {
 
-              //  Log.d(MONITOR_TAG_FOX, "Already checked " + checkWindow + ", continuing ...");
+                //  Log.d(MONITOR_TAG_FOX, "Already checked " + checkWindow + ", continuing ...");
                 continue;
-            }else{
+            } else {
                 substringsChecked.put(checkWindow, 1);
                 //Log.d(MONITOR_TAG_FOX, "Checking " + checkWindow + "!!");
             }
@@ -198,45 +204,46 @@ public class FoxDictionary implements Diction {
                     int numberCollected = thisLongest.size();
                     // TODO: This fails to return list of highest index words. First two will be sorted and the winner of this comparison will never be removed from the list, even if several subsequent higher indexes are found
                     if (numberCollected > 0) {
-                        String leastIndexWord = thisLongest.get(numberCollected-1);
+                        String leastIndexWord = thisLongest.get(numberCollected - 1);
                         String result = returnBetterIndex(leastIndexWord, candidateLongest);
-                        if(!result.equals(leastIndexWord)) {
+                        if (!result.equals(leastIndexWord)) {
 //                            Log.d(MONITOR_TAG_FOX, "LW1> Removing " + thisLongest.get(0) + ", Adding "+ res);
-                            thisLongest.remove(numberCollected-1);
+                            thisLongest.remove(numberCollected - 1);
                             thisLongest.add(result);
-                            if(thisLongest.size() < howMany){
+                            if (thisLongest.size() < howMany) {
                                 thisLongest.add(leastIndexWord);
                             }
-                        }else if(thisLongest.size() < howMany){
+                        } else if (thisLongest.size() < howMany) {
                             thisLongest.add(candidateLongest);
                         }
-                    }else{
+                    } else {
                         thisLongest.add(candidateLongest);
 //                        Log.d(MONITOR_TAG_FOX, "LW2> Adding " + candidateLongest);
                     }
 //                    Log.d(MONITOR_TAG_FOX, "Index winnder is!!: " + thisLongest);
                 }
-            }else {
+            } else {
                 thisLongest = substringSearch(checkWindow, thisLongest, Depth - 1, substringsChecked, howMany);
             }
         }
         return thisLongest;
     }
-//  Find which of two words has the highest frequency index
+
+    //  Find which of two words has the highest frequency index
     private String returnBetterIndex(String defaultLongest, String suggestedLongest) {
         String bestIndex = "";
         int defaultIndex = allValidWords.indexOf(defaultLongest);
         int suggestedIndex = allValidWords.indexOf(suggestedLongest);
-        if (defaultIndex == -1 && suggestedIndex == -1){
+        if (defaultIndex == -1 && suggestedIndex == -1) {
             return bestIndex;
         }
-        if (defaultIndex == -1 ){
+        if (defaultIndex == -1) {
             return suggestedLongest;
         }
-        if (suggestedIndex == -1 ){
+        if (suggestedIndex == -1) {
             return defaultLongest;
         }
-        if (suggestedIndex < defaultIndex){
+        if (suggestedIndex < defaultIndex) {
             return suggestedLongest;
         }
         return defaultLongest;
@@ -244,37 +251,31 @@ public class FoxDictionary implements Diction {
 
     // Randomly generate a sequence of 9 letters for the user
     public ArrayList<String> getGivenLetters() {
-        ArrayList<String> givenLetters = new ArrayList<String>();
-        String consonants = randLetter("consonants");
-        String vowels = randLetter("vowels");
-        String letters = consonants + vowels;
-        int lettersLen = letters.length();
-        for (int i = 0; i < lettersLen; i++) {
-            givenLetters.add((letters.substring(i, i + 1)));
-        }
-        shuffle(givenLetters);
-        return givenLetters;
+        int total = 9;
+        int[] options = {2, 3, 3, 3, 3, 4};
+        int random = (int) (Math.random() * options.length);
+        int vowelCount = options[random];
+        ArrayList<String> letters = new ArrayList<>();
+        letters.addAll(getRandConsonants(total - vowelCount));
+        letters.addAll(getRandVowels(vowelCount));
+        shuffle(letters);
+        return letters;
     }
-    // Generate 6 random consonants or 3 random vowels
-    private String randLetter(String choice) {
-        String letters = "";
-        String set = "";
-        int times = 0;
 
-        if (choice.equals("consonants")) {
-            times = 6;
-            int letterLen = letters.length();
-            for (int i = 0; i < times; i++) {
-                set += letterPool.getConsonant();
-            }
-        } else if (choice.equals("vowels")) {
-            times = 3;
-            int letterLen = letters.length();
-            for (int i = 0; i < times; i++) {
-                set += letterPool.getVowel();
-            }
+    private ArrayList<String> getRandConsonants(int count) {
+        ArrayList<String> consonants = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            consonants.add(letterPool.getConsonant());
         }
-        return set;
+        return consonants;
+    }
+
+    private ArrayList<String> getRandVowels(int count) {
+        ArrayList<String> vowels = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            vowels.add(letterPool.getVowel());
+        }
+        return vowels;
     }
 
     private class LetterPool {
