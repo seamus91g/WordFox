cc01535bb211dcbd42867fb52d533bbe3f6c386c
desseepe <desmondseepe@gmail.com>
2018-11-02 10:36:54 +0000

    Updated game action buttons to revert to pngs for api 19 and below

--- app/src/main/java/com/example/seamus/wordfox/game_screen/GameActivity.java
+++ app/src/main/java/com/example/seamus/wordfox/game_screen/GameActivity.java
@@ -1,8 +1,6 @@
 package com.example.seamus.wordfox.game_screen;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
@@ -12,7 +10,6 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,8 +27,6 @@ import com.example.seamus.wordfox.HomeScreen;
 import com.example.seamus.wordfox.NavigationBurger;
 import com.example.seamus.wordfox.R;
 import com.example.seamus.wordfox.RoundResults.RoundEndScreen;
-import com.example.seamus.wordfox.WifiService;
-import com.example.seamus.wordfox.WifiServiceConnection;
 import com.example.seamus.wordfox.database.FoxSQLData;
 import com.example.seamus.wordfox.injection.DictionaryApplication;
 import com.example.seamus.wordfox.profile.ProfileActivity;
@@ -101,6 +96,9 @@ public class GameActivity extends AppCompatActivity
         displayTitle();
         timeBlock = findViewById(R.id.timeBlock);
         populateTimeBlock();
+
+
+
     }
 
     // Randomly shuffle locations of the letters in the grid
